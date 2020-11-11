c57b2cc418c863afa7df57a6142828903ccfaae6
Rahmat Prastyo <rahmatprastyo88@gmail.com>
2020-11-11 18:06:38 +0700

    Login

---
+++ Test Suites/Login/Login Failed.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Gagal Login Learn Aja 
+- Data Kosong 
+- Username Salah 
+- Password Salah</description>
+   <name>Login Failed</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6131bc5c-54a5-46e9-8745-1202fe4fa66c</testSuiteGuid>
+   <testCaseLink>
+      <guid>33472875-7aa3-498c-825b-3e04686c6da5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Negative Case/Login Data Kosong</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e162e9bc-3e9f-4129-b149-dac857eb08e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Negative Case/Login Username Tidak Ada Di Database</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4cb49688-f354-414e-92b8-0efd2c624091</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Negative Case/Login Password Tidak Sama Dengan Di Database</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
