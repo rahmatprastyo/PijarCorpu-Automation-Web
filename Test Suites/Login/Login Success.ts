6761441f99fff7c5b9fd978070b0e147ea36504b
Rahmat Prastyo <rahmatprastyo88@gmail.com>
2020-11-12 11:47:23 +0700

    Login

---
+++ Test Suites/Login/Login Success.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Berhasil Login
+- Login Employee
+- Login Management 
+- Login HC 
+- Login Admin</description>
+   <name>Login Success</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>34ce80e4-d1db-45ed-ab21-8f1263bffdfd</testSuiteGuid>
+   <testCaseLink>
+      <guid>01786051-bae3-4192-96a7-ae4932aad0bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Positive Case/Login Employee</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1de38cd1-5fb6-43d1-9a53-bac7137232c8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Positive Case/Login Management</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>79bd6747-7fd8-43d5-b54f-0ebafc0a69cc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Positive Case/Login HC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7f499cda-ab04-4157-9390-54012a3da9f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Positive Case/Login Admin</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
